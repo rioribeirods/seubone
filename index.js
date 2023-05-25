26cb0f3e743c887468d73eb4ad19e5b3a7cee35f
Rio Ribeiro <rioribeirods@gmail.com>
2023-05-24 22:16:12 -0300

    adicionando rota de vendas no express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import userRoutes from "./src/routes/users.js"
+import vendaRoutes from "./src/routes/vendas.js"
 import cors from "cors"
 
 const app = express()
@@ -7,6 +8,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use("/", userRoutes)
+app.use("/", userRoutes, vendaRoutes)
 
 app.listen(8800)
