dd63197bae7703840389c1fac7083429015a8d4f
Rio Ribeiro <rioribeirods@gmail.com>
2023-05-29 20:25:25 -0300

    adicionando variáveis locais

--- src/config/populate.js
+++ src/config/populate.js
@@ -1,10 +1,17 @@
 import mysql from "mysql";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const MYSQL_USER = process.env.USERNAME;
+const MYSQL_PASSWORD = process.env.PASSWORD;
+const MYSQL_DATABASE = process.env.DATABASE;
 
 export const db = mysql.createPool({
   host: "127.0.0.1",
-  user: "rio",
-  password: "Im1ssil123",
-  database: "sistema_de_vendas",
+  user: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
 });
 
 export function populateUsers(req, res) {
